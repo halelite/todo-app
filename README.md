3a3014139e910606927d66b071e17ff97fce17d2
Hale <hale.sheikhi81@gmail.com>
2023-08-06 18:52:26 +0330

    initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# Frontend Mentor - Todo app solution
+
+This is a solution to the [Todo app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/todo-app-Su1_KokOW). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Add new todos to the list
+- Mark todos as complete
+- Delete todos from the list
+- Filter by all/active/complete todos
+- Clear all completed todos
+- Toggle light and dark mode
+- **Bonus**: Drag and drop to reorder items on the list
+
+### Screenshot
+
+![Design preview for the Interactive todo app](./design/solution-img/Screenshot2.png)
+
+### Links
+
+- Solution URL: [solution URL](https://your-solution-url.com)
+- Live Site URL: [live site URL](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+
+## Author
+
+- Frontend Mentor - [@halelite](https://www.frontendmentor.io/profile/halelite)
+- LinkedIn - [Hale Sheikhi](https://www.linkedin.com/in/hale-sheikhi/)
---
+++ script.js
@@ -0,0 +1,222 @@
+(function(){
+
+    // function for calculating left items
+    function calculateLeft() {
+        // done items
+        let n = document.getElementsByClassName('done').length;
+        // left items
+        c = todoItems.length - n;
+
+        return c;
+    }
+
+    // function for marking todos as complete
+    function markDoneJobs() {
+        for(let i = 0; i < todoBtn.length; i++) {
+            todoBtn[i].addEventListener('click', () => {
+            todoItems[i].classList.toggle('done');
+
+            leftItems.textContent = calculateLeft();
+
+            })
+        };
+    }
+
+    // function for showing cross icon in every li element once hovered
+    function showCross() {
+        if(screen.width > 440) {
+            for(let i = 0; i < todoItems.length; i++) {
+                todoItems[i].addEventListener('mouseover', () => {
+                    cross[i].style.display = 'block';
+                 })
+
+                todoItems[i].addEventListener('mouseleave', () => {
+                    cross[i].style.display = 'none';
+                })
+            };
+        }
+        
+    }
+
+    let todoBtn = document.querySelectorAll('li button');
+    let todoItems = document.querySelectorAll('li');
+    const addTodo = document.querySelector('.addTodo button');
+    const addInput = document.getElementById('addInput');
+    const filterBtns = document.querySelectorAll('.filters button');
+    const allBtn = document.getElementById('all');
+    const activeBtn = document.getElementById('active');
+    const completedBtn = document.getElementById('completed');
+    const leftItems = document.querySelector('.left');
+    const clearComplete = document.getElementById('clear');
+    const sunIcon = document.getElementById('light');
+    const moonIcon = document.getElementById('dark');
+    let cross = document.querySelectorAll('li img');
+
+    leftItems.textContent = calculateLeft();
+    
+    markDoneJobs();
+
+    // event for adding a new todo
+    addTodo.addEventListener('click', () => {
+        if(addInput.value != '') {
+            document.querySelector('ul').innerHTML += `
+                <li>
+                    <div><button type="button"></button></div>
+                    <p>${addInput.value}</p>
+                    <img src="./images/icon-cross.svg" alt="cross">
+                </li>
+            `
+            
+            // updating the number of todos
+            todoBtn = document.querySelectorAll('li button');
+            todoItems = document.querySelectorAll('li');
+            cross = document.querySelectorAll('li img');
+
+            showCross();
+           
+            leftItems.textContent = calculateLeft();
+
+            markDoneJobs();
+
+        }
+        
+        addInput.value = ''
+    });
+
+
+    showCross();
+
+    // event for deleting todos from list
+    for(let i = 0; i < cross.length; i++) {
+        cross[i].addEventListener('click', () => {
+            todoItems[i].parentElement.removeChild(todoItems[i]);
+
+            // updating the number of todos
+            todoItems = document.querySelectorAll('li');
+            leftItems.textContent = calculateLeft();
+        })
+    };
+    
+    // event for filter buttons
+    for(let i = 0; i < filterBtns.length; i++) {
+        filterBtns[i].addEventListener('click', () => {
+            for(let i = 0; i < filterBtns.length; i++) {
+                filterBtns[i].classList.remove('active');
+            }
+            filterBtns[i].classList.add('active');
+
+        })
+    };
+
+    activeBtn.addEventListener('click', () => {
+
+        for(let i = 0; i < todoItems.length; i++) {
+            if(todoItems[i].classList.contains('done')) {
+                todoItems[i].style.display = 'none';
+            } else {
+                todoItems[i].style.display = 'flex';
+            }
+        } 
+
+
+    });
+
+    completedBtn.addEventListener('click', () => {
+
+        for(let i = 0; i < todoItems.length; i++) {
+            if(!todoItems[i].classList.contains('done')) {
+                todoItems[i].style.display = 'none';
+            } else {
+                todoItems[i].style.display = 'flex';
+            }
+        }
+
+        
+    });
+
+    allBtn.addEventListener('click', () => {
+
+        for(let i = 0; i < todoItems.length; i++) {
+            todoItems[i].style.display = 'flex';
+        } 
+
+    });
+
+    // event for clearing all completed todos
+    clearComplete.addEventListener('click', () => {
+        for(let i = 0; i < todoItems.length; i++) {
+            if(todoItems[i].classList.contains('done')) {
+                todoItems[i].style.display = 'none';
+            }
+        }
+    });
+
+    // event for changing theme
+    sunIcon.addEventListener('click', () => {
+        sunIcon.style.display = 'none';
+        moonIcon.style.display = 'block';
+        document.body.classList.add('light-theme');
+    });
+
+    moonIcon.addEventListener('click', () => {
+        sunIcon.style.display = 'block';
+        moonIcon.style.display = 'none';
+        document.body.classList.remove('light-theme');
+    })
+
+        // making the list draggable
+        let items = document.querySelectorAll('ul li');
+    
+        items.forEach((item) => {
+            item.addEventListener('dragstart', handleDragStart);
+            item.addEventListener('dragend', handleDragEnd);
+            item.addEventListener('dragenter', handleDragEnter);
+            item.addEventListener('dragleave', handleDragLeave);
+            item.addEventListener('drop', handleDrop);
+            item.addEventListener('dragover', handleDragOver);
+        });
+
+        function handleDragStart(e) {
+            this.style.opacity = '0.4';
+
+            dragSrcEl = this;
+
+            e.dataTransfer.effectAllowed = 'move';
+            e.dataTransfer.setData('text/html', this.innerHTML);
+        }
+
+        function handleDragEnd(e) {
+            this.style.opacity = '1';
+        }
+
+        function handleDragEnter(e) {
+            this.classList.add('over');   
+        }
+
+        function handleDragLeave(e) {
+            this.classList.remove('over');
+        }
+
+        function handleDrop(e) {
+
+            e.stopPropagation();  // stops the browser from redirecting
+
+            if(dragSrcEl !== this) {
+                dragSrcEl.innerHTML = this.innerHTML;
+                this.innerHTML = e.dataTransfer.getData('text/html');
+            }
+
+            setTimeout(() => {
+                this.classList.remove('over');
+            }, 200);
+        }
+
+        // prevent the default action for dragover which sets the dropEffect to 'none'
+        function handleDragOver(e) {
+            e.preventDefault();  // 
+        }
+
+    
+
+
+}) ();
